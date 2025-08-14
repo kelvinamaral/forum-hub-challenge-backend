3630e17e707c76d56ea7b8fe6d683ef5edf1f4cc
Kelvin <kelvinfamaral@hotmail.com>
2025-08-14 15:29:00 -0300

    Adicionado arquivos do Spring Initializer para iniciar o projeto

---
+++ src/test/java/com/alura/desafio_forum_hub/DesafioForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.desafio_forum_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
