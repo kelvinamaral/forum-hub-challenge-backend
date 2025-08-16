0af6acb2bac327bcf7db40d4a1f18c20b5b7d624
Kelvin <kelvinfamaral@hotmail.com>
2025-08-16 18:23:14 -0300

    Ajuste no UsuarioController.java

--- src/main/java/com/alura/desafio_forum_hub/domain/usuario/dto/DetalhesUsuarioDTO.java
+++ src/main/java/com/alura/desafio_forum_hub/domain/usuario/dto/DetalhesUsuarioDTO.java
@@ -1,12 +1,26 @@
 package com.alura.desafio_forum_hub.domain.usuario.dto;
 
 import com.alura.desafio_forum_hub.domain.usuario.Role;
+import com.alura.desafio_forum_hub.domain.usuario.Usuario;
 
-public record DetalhesUsuarioDTO(Long id,
-                                 String username,
-                                 Role role,
-                                 String nome,
-                                 String sobrenome,
-                                 String email,
-                                 Boolean enabled) {
+public record DetalhesUsuarioDTO(
+        Long id,
+        String username,
+        Role role,
+        String nome,
+        String sobrenome,
+        String email,
+        Boolean enabled
+) {
+    public DetalhesUsuarioDTO(Usuario usuario) {
+        this(
+                usuario.getId(),
+                usuario.getUsername(),
+                usuario.getRole(),
+                usuario.getNome(),
+                usuario.getSobrenome(),
+                usuario.getEmail(),
+                usuario.getEnabled()
+        );
+    }
 }
